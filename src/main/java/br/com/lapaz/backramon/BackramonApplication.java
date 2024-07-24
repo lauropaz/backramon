5957dc893a47e4c16e068b7ca016779eeb6f07b8
lauropaz <lauroskd@gmail.com>
2024-07-24 07:56:59 -0300

    Add projeto

---
+++ src/main/java/br/com/lapaz/backramon/BackramonApplication.java
@@ -0,0 +1,13 @@
+package br.com.lapaz.backramon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackramonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackramonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lapaz/backramon/BackramonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lapaz.backramon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackramonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
